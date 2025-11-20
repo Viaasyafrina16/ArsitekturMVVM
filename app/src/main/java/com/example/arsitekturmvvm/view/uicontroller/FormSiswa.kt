3c934d44aad1f1f039a10f5ae5c244887b7794dc
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-20 16:09:21 +0700

    Mengganti warna text black

--- app/src/main/java/com/example/arsitekturmvvm/view/uicontroller/FormSiswa.kt
+++ app/src/main/java/com/example/arsitekturmvvm/view/uicontroller/FormSiswa.kt
@@ -54,7 +54,7 @@ fun FormSiswa(
                 title = {
                     Text(
                         text = stringResource(id = R.string.app_name),
-                        color = Color.White
+                        color = Color.Black
                     )
                 },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_500))
