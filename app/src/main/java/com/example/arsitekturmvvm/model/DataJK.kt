6dda163f20f4942fa2e6d64a6befba9254c0daa4
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-20 15:57:32 +0700

    Menambahkan DataJK untuk daftar jenis kelamin

---
+++ app/src/main/java/com/example/arsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,11 @@
+package com.example.arsitekturmvvm.model
+
+
+import com.example.arsitekturmvvm.R
+
+object DataJK {
+    val Jenis = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
