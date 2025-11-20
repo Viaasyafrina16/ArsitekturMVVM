787e5876e74909a673727696f6af3191a8fb73c6
Viaasyafrina16 <syafrinavia@gmail.com>
2025-11-20 16:03:17 +0700

    membuat form navigasi untuk memanggil halaman navigasi

---
+++ app/src/main/java/com/example/arsitekturmvvm/Navigasi.kt
@@ -0,0 +1,72 @@
+package com.example.arsitekturmvvm
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.arsitekturmvvm.model.DataJK
+import com.example.arsitekturmvvm.view.uicontroller.FormSiswa
+import com.example.arsitekturmvvm.viewmodel.SiswaViewModel
+import com.example.arsitekturmvvm.view.uicontroller.TampilSiswa
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    //edit 1 : parameter viewModel
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    Scaffold { isiRuang ->
+        //edit 2 : Tambahkan variabel uiState
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)) {
+            composable(route = Navigasi.Formulir.name) {
+                //edit 3 : Tambahkan variable konteks
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = DataJK.Jenis.map { id ->
+                        konteks.getString(id)
+                    },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+                    //edit 5 : parameter statusUiSiswa
+                    statusUiSiswa = uiState.value,
+                    onBackButtonClicked = { cancelAndBackToFormulir(navController) }
+                )
+            }
+        }
+    }
+}
+
+
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulir.name, inclusive = false)
+}
